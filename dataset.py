fd2d537f96ea33493736372eb035a3c3293b2be4
abdul vajid <vajidca444@gmail.com>
2025-10-04 21:45:22 +0530

    dataset hyperparemetrers

--- dataset.py
+++ dataset.py
@@ -1,21 +1,16 @@
 from torch.utils.data import Dataset, DataLoader
 from pathlib import Path
-from PIL.Image import Image
+from PIL import Image
+import numpy as np
 
 class ImgDataset(Dataset):
-    def __init__(self, path, is_eval=False):
+    def __init__(self, path, transform=None):
         super().__init__()
-        self.is_eval = is_eval
         self.path = Path(path)
+        self.images =  (self.path).glob("img/*.png")
+        self.labels = (self.path).glob("label/*.png")
 
-        if not is_eval:
-            self.images =  (self.path).glob("train/img/*.png")
-            self.labels = (self.path).glob("train/label/*.png")
-        else:
-            self.images =  (self.path).glob("val/img/*.png")
-            self.labels = (self.path).glob("val/label/*.png")
-
-        self.transform = 
+        self.transform = transform
 
 
     def __len__(self):
@@ -23,6 +18,14 @@ class ImgDataset(Dataset):
 
     def __getitem__(self, index):
         img_path = self.images[index]
-        pil_img = Image.load(img_path)
+        label_path = self.labels[index]
+        image = np.array(Image.open(img_path).convert("RGB"))
+        label = np.array(Image.open(label_path).convert("RGB"))
+
+        if self.transform is not None:
+            augmentation = self.transform(image=image, label=label)
+            image, label = augmentation['image'], augmentation['label']
+        
+        return image, label 
 
         
--- train.py
+++ train.py
@@ -1,31 +1,78 @@
+from pickle import TRUE
+from sympy import Limit
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 from model import UNET
-from dataset import get_dataloader
+from dataset import ImgDataset
+import albumentations as A
+from torch.utils.data import DataLoader
+from albumentations.pytorch import ToTensorV2
 
 
-def eval(model, optimizer)
+LEARNING_RATE = 1e-4
+DEVICE = 'cuda' if torch.cuda.is_available() else ('mps' if torch.mps.is_available() else 'cpu')
+BATCH_SIZE = 16
+NUM_EPOCHS = 100
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 512
+IMAGE_WIDTH = 512
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_DATA_PATH = 'data/train'
+VAL_DATA_PATH = 'data/val'
+WEIGHT_DECAY = 0.0
 
-def train(model, optimizer):
+def train(model, train_loader, val_loader, optimizer):
+    loop = tqdm(train_loader, dynamic_ncols=True)
 
-    # TODO : Add autocast bfloat training
-
-    progress_bar = tqdm(dataloader)
-    model.train()
-    for inputs, targets in progress_bar:
-        _ ,loss = model(inputs, targets)
+    for inputs, targets in loop:
+        model.train()
+        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+        # with torch.amp.autocast(DEVICE, dtype=torch.bfloat16):
+        #     loss = model(inputs, targets) 
+        loss = model(inputs, targets) 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
+
+        loop.set_postfix(loss=loss.item())
+
 
 def main():
-    model = UNET()
-    loader = get_dataloader()
-    optimizer = optim.Adam(model.parameters())
-    num_epoches = 100
+    model = UNET().to(DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
 
-    for epoch in num_epoches:
-        train(model, optimizer, loader)
+    train_transform = A.Compose(
+        A.Resize(IMAGE_HEIGHT, IMAGE_WIDTH),
+        A.Rotate(limit=10, p=0.7),
+        A.HorizontalFlip(),
+        A.Normalize(
+            mean=[0.0, 0.0, 0.0],
+            std=[1.0, 1.0, 1.0],
+            max_pixel_value=255.0
+        ),
+        ToTensorV2()
+    )
+
+    val_transform = A.Compose(
+        A.Resize(IMAGE_HEIGHT, IMAGE_WIDTH),
+        A.Normalize(
+            mean=[0.0, 0.0, 0.0],
+            std=[1.0, 1.0, 1.0],
+            max_pixel_value=255.0
+        ),
+        ToTensorV2()
+    )
+
+    train_dataset = ImgDataset(TRAIN_DATA_PATH, transform=train_transform)
+    val_dataset = ImgDataset(VAL_DATA_PATH, transform=val_transform)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
+
+    # TODO Load model if exist
+
+    # model.compile()
+    for epoch in range(NUM_EPOCHS):
+        train(model=model, train_loader=train_loader, val_loader=val_loader, optimizer=optimizer)
