da253608e055a4b40fa67de12e2e30697c53611b
abdul vajid <vajidca444@gmail.com>
2025-10-04 15:55:41 +0530

    code cleanup

--- model.py
+++ model.py
@@ -1,7 +1,5 @@
-from os import name
-from sympy import riemann_xi
 import torch
-from torch import Tensor, mode
+from torch import Tensor
 import torch.nn as nn
 from torchvision.transforms import Resize 
 
@@ -27,13 +25,15 @@ class UNET(nn.Module):
         super().__init__()
         self.conv_downs = nn.ModuleList()
         self.conv_ups = nn.ModuleList()
-        in_channels = 3
+        in_channels = 1
 
+        # Module list U down part
         for out_channels in features:
             self.conv_downs.append(UnetConv(in_channels=in_channels, out_channels=out_channels))
             self.conv_downs.append(nn.MaxPool2d(kernel_size=2))
             in_channels = out_channels
 
+        # Module list U up part
         for out_channels in reversed(features):
             in_channels = out_channels*2 # skip connection, add dim
             self.conv_ups.append(UnetConv(in_channels=in_channels, out_channels=out_channels))
@@ -53,54 +53,37 @@ class UNET(nn.Module):
     
     def forward(self, x: Tensor):
         out_features = []
+
         for module in self.conv_downs:
             x = module(x)
-            print("down shape", x.shape)
             if isinstance(module, UnetConv):
                 out_features.append(x)
-        
-        x = self.bottleneck_layer(x)
-        print(x.shape,'bottleneck')
-        out_features = out_features[::-1]
 
+        x = self.bottleneck_layer(x)
+
+        out_features = out_features[::-1]
         i = 0
         for module in self.conv_ups:
-            # import code; code.interact(local=locals())
             # resize out_feature value to x same shape
             if isinstance(module, UnetConv):
                 height, width = x.shape[-2], x.shape[-1]
                 resized_features = Resize((height, width))(out_features[i])
                 i+=1
-                print(x.shape)
-                print(resized_features.shape)
                 assert x.shape[1] == resized_features.shape[1] and x.shape[-1] == resized_features.shape[-1], "size should match"
-                x = torch.concat([x, resized_features], dim=1)
-                print(f'x after concat {x.shape}')
-                
+                x = torch.concat([x, resized_features], dim=1)    
             x = module(x)
-            print("up", x.shape)
-        
+
         return self.output_layer(x)
                 
 
-
-        
-
-
-    
-
-
-
-
-
 def main():
-    x = torch.rand((1, 3, 572, 572))
-    conv = UnetConv(3, 64)
+    x = torch.rand((1, 1, 572, 572))
+    conv = UnetConv(1, 64)
     print(f"UnetConv{x.shape} -> unet_conv -> {conv(x).size()}")
 
     model = UNET()
 
-    print(f"{x.size()} -> model(x) -> {model(x).size()}")
+    print(f"UNET MODEL {x.size()} -> model(x) -> {model(x).size()}")
 
 
 if __name__ == '__main__':
